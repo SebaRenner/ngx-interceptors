d2767b313be7aeb15088bfd3dadabb479964029b
SebaRenner <sebastian.renner@gmx.ch>
2024-03-21 22:22:31 +0100

    Added further header interceptor unit tests

--- projects/ngx-interceptors/src/lib/header/header.interceptor.spec.ts
+++ projects/ngx-interceptors/src/lib/header/header.interceptor.spec.ts
@@ -3,14 +3,17 @@ import { HeaderInterceptor } from "./header.interceptor";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 import { HEADER_INTERCEPTOR_CONFIG, HttpHeader } from "./header.config";
+import { DynamicHeaderService } from "./services/dynamic-header.service";
+
+class DynamicHeaderServiceMock {
+  getHeaders() {
+    return { 'Dynamic-Header': 'dynamic-value' }
+  }
+}
 
 describe('HeaderInterceptor', () => {
   let httpClient: HttpClient;
   let httpMock: HttpTestingController;
-
-  const headers: HttpHeader = {
-    'Content-Type': 'application/json'
-  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,26 +25,39 @@ describe('HeaderInterceptor', () => {
           multi: true
         },
         {
-          provide: HEADER_INTERCEPTOR_CONFIG,
-          useValue: {
-            headers: headers
-          }
+          provide: DynamicHeaderService,
+          useClass: DynamicHeaderServiceMock
         }
       ]
     });
-
-    httpClient = TestBed.inject(HttpClient);
-    httpMock = TestBed.inject(HttpTestingController);
   });
 
   afterEach(() => {
     httpMock.verify();
   });
 
+  it('should use default config', () => {
+    // arrange
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    // act
+    httpClient.get('https://example.com').subscribe();
+
+    // assert
+    const req = httpMock.expectOne('https://example.com');
+    expect(req.request.headers.keys()).toEqual([]);
+  })
+
   it('should add headers to request', () => {
     // arrange
     const name = 'Content-Type';
-    const value = 'application/json'
+    const value = 'application/json';
+    const headers: HttpHeader = { [name]: value };
+
+    TestBed.overrideProvider(HEADER_INTERCEPTOR_CONFIG, { useValue: { headers: headers } });
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
 
     // act
     httpClient.get('https://example.com').subscribe();
@@ -51,5 +67,54 @@ describe('HeaderInterceptor', () => {
     const header = req.request.headers.get(name);
     expect(header).toBeDefined();
     expect(header).toBe(value);
+  })
+
+  it('should use dynamic headers', () => {
+    // arrange
+    TestBed.overrideProvider(HEADER_INTERCEPTOR_CONFIG, { useValue: { enableDynamicHeaders: true } });
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    // act
+    httpClient.get('https://example.com').subscribe();
+
+    // assert
+    const req = httpMock.expectOne('https://example.com');
+    const header = req.request.headers.get('Dynamic-Header');
+    expect(header).toBeDefined();
+    expect(header).toBe('dynamic-value');
+  })
+
+  it('should not use dynamic headers', () => {
+    // arrange
+    TestBed.overrideProvider(HEADER_INTERCEPTOR_CONFIG, { useValue: { enableDynamicHeaders: false } });
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    // act
+    httpClient.get('https://example.com').subscribe();
+
+    // assert
+    const req = httpMock.expectOne('https://example.com');
+    const header = req.request.headers.get('Dynamic-Header');
+    expect(header).toBeNull();
+  })
+
+  it('should merge static and dynamic headers', () => {
+    // arrange
+    const name = 'Content-Type';
+    const value = 'application/json';
+    const headers: HttpHeader = { [name]: value };
+
+    TestBed.overrideProvider(HEADER_INTERCEPTOR_CONFIG, { useValue: { headers: headers, enableDynamicHeaders: true } });
+    httpClient = TestBed.inject(HttpClient);
+    httpMock = TestBed.inject(HttpTestingController);
+
+    // act
+    httpClient.get('https://example.com').subscribe();
+
+    // assert
+    const req = httpMock.expectOne('https://example.com');
+    expect(req.request.headers.keys()).toEqual(['Content-Type', 'Dynamic-Header']);
   })
 });
